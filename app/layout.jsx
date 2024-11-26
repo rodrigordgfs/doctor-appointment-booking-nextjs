20db8efd74eb83a62614e3b40899f11c6c3660ec
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2024-11-26 00:41:06 -0300

    feature: Adicionado custom font

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,16 +1,9 @@
-import localFont from "next/font/local";
+import { Outfit } from "next/font/google";
 import "./globals.css";
 import Header from "./_components/Header";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const outfit = Outfit({
+  subsets: ["latin", "latin-ext"],
 });
 
 export const metadata = {
@@ -21,12 +14,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${outfit.className} antialiased`}>
         <div className="md:px-20">
-        <Header />
-        {children}
+          <Header />
+          {children}
         </div>
       </body>
     </html>
